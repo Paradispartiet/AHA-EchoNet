494e2bed1721f8d724e22b9dd245d5f2d53a967b
Paradispartiet <paradispartiet@gmail.com>
2025-12-05 11:45:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 // Enkel service worker for AHA Chat – offline + cache av app-shell
 
-const CACHE_NAME = "aha-chat-v1.0.0.108";
+const CACHE_NAME = "aha-chat-v1.0.0.109";
 
 // Justér stier hvis nettstedet ligger i en undermappe
 const ASSETS = [
