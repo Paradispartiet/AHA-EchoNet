ae0c24735bd7d567d76f67847ad97044564fe6ba
Paradispartiet <paradispartiet@gmail.com>
2026-05-07 15:34:41 +0200

    Set production auth redirect URL

--- ahaConfig.js
+++ ahaConfig.js
@@ -2,5 +2,7 @@
 // Runtime configuration for AHA-EchoNet.
 // Public Supabase frontend config only. Do not put database passwords or service-role keys here.
 
+window.AHA_APP_URL = window.AHA_APP_URL || "https://paradispartiet.github.io/AHA-EchoNet/";
+window.AHA_AUTH_REDIRECT_URL = window.AHA_AUTH_REDIRECT_URL || window.AHA_APP_URL;
 window.AHA_SUPABASE_URL = window.AHA_SUPABASE_URL || "https://wshmybqyksrwkawqleiz.supabase.co";
 window.AHA_SUPABASE_PUBLISHABLE_KEY = window.AHA_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_fgfxuPJBpZ9CFcYufBBgjg_8YEmi13m";
