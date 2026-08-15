f280d680cc448e8aa057e961f97f964feac9afe2
Paradispartiet <paradispartiet@gmail.com>
2026-08-15 12:31:33 +0200

    Add canonical sync API module

---
+++ backend/api/src/sync/sync.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { CANONICAL_SYNC_CONFIG, loadCanonicalSyncConfig } from "./sync.config.js";
+import { CanonicalSyncController } from "./sync.controller.js";
+import { CanonicalSyncRepository } from "./sync.repository.js";
+import { CanonicalSyncService } from "./sync.service.js";
+
+@Module({
+  controllers: [CanonicalSyncController],
+  providers: [
+    {
+      provide: CANONICAL_SYNC_CONFIG,
+      useFactory: () => loadCanonicalSyncConfig()
+    },
+    CanonicalSyncRepository,
+    CanonicalSyncService
+  ]
+})
+export class CanonicalSyncModule {}
